26dd1bf7ae24c8e48c16140d8ab5434ed43bb6ca
Nguyen Quoc Dinh <nqdinh83@gmail.com>
2018-08-21 21:48:03 +0700

    -remove tests

--- TestFolder/TestScript.js
+++
@@ -1,9 +0,0 @@
-handlers.UnitUpdateTest = function (args, context) {
-    var message = "Hello " + currentPlayerId + "!";
-    log.info(message);
-    var inputValue = null;
-    if (args && args.hasOwnProperty("inputValue"))
-        inputValue = args.inputValue;
-    log.debug("helloWorld:", { input: inputValue });
-    return { messageValue: message };
-};
