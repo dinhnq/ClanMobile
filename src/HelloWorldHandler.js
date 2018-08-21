eac09feda17ed508f862ab4a23c5ed7aba24b422
Nguyen Quoc Dinh <nqdinh83@gmail.com>
2018-08-21 20:48:00 +0700

    HelloWorld content

--- src/HelloWorldHandler.js
+++ src/HelloWorldHandler.js
@@ -1,5 +1,10 @@
-// Cloud Script (javascript)
-handlers.helloWorld = function (args, context)
-{
-	
+// Cloud Script (JavaScript)
+handlers.helloWorld = function (args, context) {
+    var message = "Hello " + currentPlayerId + "!";
+    log.info(message);
+    var inputValue = null;
+    if (args && args.hasOwnProperty("inputValue"))
+        inputValue = args.inputValue;
+    log.debug("helloWorld:", { input: inputValue });
+    return { messageValue: message };
 }
